00532bcc56fc6ae508c3e906308466aaa18bc9cb
Ludovic SANSONE <libreaudio63@gmail.com>
2022-12-28 18:49:18 +0100

    Modification de la configuration globale

--- LITReview/LITReview/urls.py
+++ LITReview/LITReview/urls.py
@@ -1,21 +1,16 @@
-"""LITReview URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from main import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_views.signin, name='signin'),
+    path('signup', main_views.signup, name='signup'),
+    path('home', main_views.home, name='home'),
+    path('subscribs', main_views.subscribs, name='subscribs'),
+    path('contributions', main_views.contributions, name='contributions'),
+    path('new-ticket', main_views.new_ticket, name='new-ticket'),
+    path('edit-ticket', main_views.edit_ticket, name='edit-ticket'),
+    path('new-review', main_views.new_review, name='new-review'),
+    path('edit-review', main_views.edit_review, name='edit-review'),
 ]
